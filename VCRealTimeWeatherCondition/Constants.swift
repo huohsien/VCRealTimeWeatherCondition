a3100bd73ca10bced481e2f3d8a10f14e3e7348b
Victor Jiang <huohsien@me.com>
2018-04-24 19:26:07 +0800

    add Kanna module. testing layout and some testing code

---
+++ VCRealTimeWeatherCondition/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  VCRealTimeWeatherCondition
+//
+//  Created by victor on 2018/4/20.
+//  Copyright © 2018 VHHC Studio. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let baseUrl = "https://www.cwb.gov.tw/V7/observe/real/46692.htm?"
+}
--- VCRealTimeWeatherCondition/ViewController.swift
+++ VCRealTimeWeatherCondition/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import WebKit
+import Kanna
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    @IBOutlet weak var relHumidLabel: UILabel!
+    
+    @IBOutlet weak var loadingPageIndicator: UIActivityIndicatorView!
+    
+    var wkWebView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        loadWKWebView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - webkit functions
+    
+    func loadWKWebView() {
+        
+        let webViewRatio: CGFloat = 0.35
+        wkWebView = WKWebView(frame: CGRect(x: 0.0, y: view.bounds.size.height * (1.0 - webViewRatio), width: view.bounds.size.width, height: view.bounds.size.height * webViewRatio))
+        wkWebView.navigationDelegate = self
+        wkWebView.alpha = 0.0
+        self.view.addSubview(wkWebView)
+        guard let url = URL(string: Constants.baseUrl) else {
+            print("failed to create url")
+            return
+        }
+        let urlRequest = URLRequest(url: url)
+        wkWebView.load(urlRequest)
+        loadingPageIndicator.startAnimating()
     }
-
-
+    
+    //MARK: - webkit callbacks
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        loadingPageIndicator.stopAnimating()
+        loadingPageIndicator.hidesWhenStopped = true
+        wkWebView.alpha = 1.0
+        
+        currentTemperature()
+    }
+    
+    //MARK: - html parser
+    
+    func currentTemperature() {
+        
+        guard let url = URL(string: Constants.baseUrl) else {
+            print("Error: \(Constants.baseUrl) is not a valid URL")
+            return
+        }
+        
+        do {
+            let htmlString = try String(contentsOf: url, encoding: .utf8)
+            print("htmlString=\(htmlString)")
+        } catch let error {
+            print("Error: \(error)")
+        }
+    }
+    
 }
 
