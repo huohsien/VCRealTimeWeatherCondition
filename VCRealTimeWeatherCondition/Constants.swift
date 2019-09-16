ccbd5a52640d12432e80bfb7c68cdcb333cb470c
Victor Jiang <huohsien@me.com>
2019-09-17 03:01:45 +0800

    add rainfall information retrieved from another weather webpage. use a better way of searching xml element using xpath

--- VCRealTimeWeatherCondition/Constants.swift
+++ VCRealTimeWeatherCondition/Constants.swift
@@ -12,6 +12,7 @@ struct Constants {
     
 //    static let baseUrl = "https://www.cwb.gov.tw/V7/observe/real/46692.htm?"
     static let baseUrl = "https://www.cwb.gov.tw/V7/observe/real/NewObs.htm?stationID=CAAH6&idx=63#ui-tabs-1"
+    static let baseUrl2 = "https://www.cwb.gov.tw/V7/observe/rainfall/A136.htm"
 
     
 }
--- VCRealTimeWeatherCondition/ViewController.swift
+++ VCRealTimeWeatherCondition/ViewController.swift
@@ -22,15 +22,24 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     @IBOutlet weak var dateTimeLabel: UILabel!
     
+    @IBOutlet weak var rainfallLabel: UILabel!
+    
     @IBOutlet weak var locationNameLabel: UILabel!
     
     var wkWebView: WKWebView!
-    
+    var wkWebView2: WKWebView!
+    var isTempHumidDataRetrieved: Bool!
+    var isRainfallDataRetrieved: Bool!
+
     var html: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        isTempHumidDataRetrieved = false
+        isRainfallDataRetrieved = false
+
+        removeWKWebViewCookies()
         loadWKWebView()
     }
 
@@ -39,9 +48,11 @@ class ViewController: UIViewController, WKNavigationDelegate {
     func loadWKWebView() {
         
         let webViewRatio: CGFloat = 0.35
+        
         wkWebView = WKWebView(frame: CGRect(x: 0.0, y: view.bounds.size.height * (1.0 - webViewRatio), width: view.bounds.size.width, height: view.bounds.size.height * webViewRatio))
         wkWebView.navigationDelegate = self
         wkWebView.alpha = 0.0
+        
         self.view.addSubview(wkWebView)
         guard let url = URL(string: Constants.baseUrl) else {
             print("failed to create url")
@@ -50,19 +61,33 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let urlRequest = URLRequest(url: url)
         wkWebView.load(urlRequest)
         loadingPageIndicator.startAnimating()
+        
+        // for getting rainfall
+        //
+        wkWebView2 = WKWebView(frame: CGRect(x: 0.0, y: view.bounds.size.height * (1.0 - webViewRatio), width: view.bounds.size.width, height: view.bounds.size.height * webViewRatio))
+        wkWebView2.navigationDelegate = self
+        wkWebView2.alpha = 0.0
+        self.view.addSubview(wkWebView2)
+        guard let url2 = URL(string: Constants.baseUrl2) else {
+            print("failed to create url")
+            return
+        }
+        let urlRequest2 = URLRequest(url: url2)
+        wkWebView2.load(urlRequest2)
+        loadingPageIndicator.startAnimating()
     }
     
     //MARK: - webkit callbacks
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        loadingPageIndicator.stopAnimating()
-        loadingPageIndicator.hidesWhenStopped = true
         
-        
-        // SHOW THE WEBPAGE FOR DEBUG OR NOT
-//        wkWebView.alpha = 1.0
-        
-        fetchAndUpdateData()
+        if webView == wkWebView {
+            print("callback didFinish from wkWebView")
+            fetchAndUpdateData()
+        } else if webView == wkWebView2 {
+            print("callback didFinish from wkWebView2")
+            fetchAndUpdateData2()
+        }
     }
     
     //MARK: - html parser
@@ -72,7 +97,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         // get the result of the requested page
         wkWebView.evaluateJavaScript("document.documentElement.outerHTML.toString()") { (result, error) in
 
-            self.html = result as! String
+            self.html = result as? String
             
             // parsing
             do {
@@ -118,7 +143,11 @@ class ViewController: UIViewController, WKNavigationDelegate {
                                     
                                     self.dateTimeLabel.text = dateTimeString
                                     self.locationLabel.text = locationNameString
-                                    
+                                    self.isTempHumidDataRetrieved = true
+                                    if self.isRainfallDataRetrieved {
+                                        self.loadingPageIndicator.stopAnimating()
+                                        self.loadingPageIndicator.hidesWhenStopped = true
+                                    }
                                     return
                                 }
                             }
@@ -143,6 +172,75 @@ class ViewController: UIViewController, WKNavigationDelegate {
 //                    }
 //                }
             } catch {/* error handling here */}
+        }
+    }
+    
+    func fetchAndUpdateData2() {
+        
+        // get the result of the requested page
+        wkWebView2.evaluateJavaScript("document.documentElement.outerHTML.toString()") { (result, error) in
+            
+            self.html = result as? String
+            
+            // parsing
+            do {
+                let doc = try HTML(html: self.html, encoding: .utf8)
+                for data in doc.xpath("//*[@id=\"tableData\"]//*[.=\"臺灣大學 (A0A01)\"]/../../td[3]") {
+                    guard let rainfallString: String = data.text else {continue}
+                        print(rainfallString)
+                    if rainfallString == "-" {
+                        self.rainfallLabel.text = "0.0"
+                    } else if rainfallString == "X" {
+                        
+                        // if the data mearured at NTU site is not available, try to get the data at Daan forest Park
+                        //
+                        for data in doc.xpath("//*[@id=\"tableData\"]//*[.=\"大安森林 (CAAH6)\"]/../../td[3]") {
+                            guard let rainfallString: String = data.text else {continue}
+//                            print(rainfallString)
+                            if rainfallString == "-" {
+                                self.rainfallLabel.text = "0.0"
+                            } else if rainfallString == "X" {
+                                self.rainfallLabel.text = "Unknown"
+                            } else {
+                                self.rainfallLabel.text = rainfallString
+                            }
+                        }
+                    } else {
+                        self.rainfallLabel.text = rainfallString
+                    }
+                }
+
+            } catch {/* error handling here */}
+            self.isRainfallDataRetrieved = true
+            if self.isTempHumidDataRetrieved {
+                self.loadingPageIndicator.stopAnimating()
+                self.loadingPageIndicator.hidesWhenStopped = true
+            }
+        }
+    }
+    // MARK: - helper functions
+    
+    func removeWKWebViewCookies() {
+        
+        //iOS9.0以上使用的方法
+        if #available(iOS 9.0, *) {
+            let dataStore = WKWebsiteDataStore.default()
+            dataStore.fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes(), completionHandler: { (records) in
+                for record in records{
+                    //清除本站的cookie
+                    if record.displayName.contains("sina.com"){//这个判断注释掉的话是清理所有的cookie
+                        WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {
+                            //清除成功
+                            print("清除成功\(record)")
+                        })
+                    }
+                }
+            })
+        } else {
+            //ios8.0以上使用的方法
+            let libraryPath = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.libraryDirectory, FileManager.SearchPathDomainMask.userDomainMask, true).first
+            let cookiesPath = libraryPath! + "/Cookies"
+            try!FileManager.default.removeItem(atPath: cookiesPath)
         }
     }
 }
